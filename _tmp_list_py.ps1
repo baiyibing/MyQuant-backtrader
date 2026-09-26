d4689f740a78ed26e39a8bb8994eec1b9fb6fe8f
byb <94577518@qq.com>
2026-09-26 10:44:03 +0800

    chore: drop 91 unrelated scratch files from PR #211; gitignore _agent_runs/_tmp_/*.py.bak_*

--- _tmp_list_py.ps1
+++
@@ -1,8 +0,0 @@
-﻿Get-CimInstance Win32_Process -Filter "Name='python.exe'" | ForEach-Object {
-  $mb = [math]::Round($_.WorkingSetSize/1MB,0)
-  $cl = $_.CommandLine
-  if (-not $cl) { $cl = '' }
-  $n = [Math]::Min(350, $cl.Length)
-  $cmd = $cl.Substring(0, $n)
-  Write-Output ("{0}`t{1}MB`t{2}" -f $_.ProcessId, $mb, $cmd)
-}
